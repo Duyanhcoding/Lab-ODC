55a5ea9e48eb585eee8cb053820d0dbd8f0715e8
van7930 <van79303@gmail.com>
2026-02-02 13:40:37 +0700

    fix

--- client/src/pages/Unauthorized.tsx
+++ client/src/pages/Unauthorized.tsx
@@ -2,7 +2,7 @@ export default function Unauthorized() {
   return (
     <div style={{ padding: 50 }}>
       <h1>403 - Forbidden</h1>
-      <p>Bạn không có quyền truy cập trang này.</p>
+      <p>You are not authorized to access this page.</p>
     </div>
   );
 }
